94a7fd05f52c4a98e6a7b7b3039a4d8a6cce7966
Morgan Benton <morgan.benton@gmail.com>
2021-01-07 19:39:28 -0500

    feat(users): add users service
    Added an API endpoint `/users` for managing user data. Configured mongoose to talk to MongoDB Atlas.
    Setup local SSL and updated `README.md` to explain how to get the dev environment setup.

---
+++ config/default.js
@@ -0,0 +1,10 @@
+module.exports = {
+  host: 'api.blackjack.test',
+  port: 3030,
+  public: '../public/',
+  paginate: {
+    default: 10,
+    max: 50,
+  },
+  mongodb: `mongodb+srv://${process.env.BLACKJACK_DEV_USER}:${process.env.BLACKJACK_DEV_PW}@cluster0.4yurq.mongodb.net/${process.env.BLACKJACK_DEV_DB}?retryWrites=true&w=majority`,
+}
